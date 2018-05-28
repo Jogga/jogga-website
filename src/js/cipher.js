691365e98d6ac16fe2d788645132a64e0568d95d
Joachim Fröstl <joachim@froestl.com>
2018-05-28 11:35:46 +0200

    Improve contact cipher

---
+++ src/js/cipher.js
@@ -0,0 +1,16 @@
+function rot13(match, p1, p2, offset, string) {
+	return match.replace(/[a-z]/gi , function(c) {
+		return String.fromCharCode(c.charCodeAt(0) + (c.toUpperCase() <= "M" ? 13 : -13));
+	});
+}
+function rot5(match, p1, p2, offset, string) {
+	return match.replace(/\d/gi , function(c) {
+		return String.fromCharCode(c.charCodeAt(0) + (c <= "4" ? 5 : -5));
+	});
+}
+function cipherMail(mail) {
+	return mail.replace(/[a-z]+(?=@)|[a-z]+(?=\.)/gi, rot13);
+}
+function cipherPhone(phone) {
+	return phone.replace(/\d+$/g, rot5);
+}
